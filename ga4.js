5d8b66e3a8342230e4810ffab4b338ac23371e23
geziplatformu <futbolsovu@gmail.com>
2026-08-19 10:16:19 +0300

    Add Google Analytics 4 loader

---
+++ ga4.js
@@ -0,0 +1,12 @@
+// Gezi Platformu - Google Analytics 4
+(function () {
+  var measurementId = 'G-SL0GWRTHRR';
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function(){ window.dataLayer.push(arguments); };
+  window.gtag('js', new Date());
+  window.gtag('config', measurementId);
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=' + encodeURIComponent(measurementId);
+  document.head.appendChild(script);
+})();
