9b792a11233d4a0755530e8ffe52f9d9cf18cd31
geziplatformu <futbolsovu@gmail.com>
2026-08-22 03:24:22 +0300

    Instagram profil ekranını sabitle

---
+++ instagram-static.js
@@ -0,0 +1,5 @@
+(()=>{
+  const phone=document.getElementById('instagramPhone');
+  if(!phone)return;
+  phone.innerHTML=`<a class="instagram-static-screen" href="https://www.instagram.com/geziplatformuu/" target="_blank" rel="noopener" aria-label="Gezi Platformu Instagram profilini aç"><img src="assets/instagram/geziplatformuu-profile.jpg" alt="Gezi Platformu Instagram profil ekranı"><span class="instagram-static-hint">Instagram profilini açmak için dokunun</span></a>`;
+})();
