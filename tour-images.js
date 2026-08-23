4509df657bd12e8fa3013c067f5e3938309cc685
geziplatformu <futbolsovu@gmail.com>
2026-08-24 00:24:40 +0300

    Fix Kahramanmaras cover override

--- tour-images.js
+++ tour-images.js
@@ -1,6 +1,5 @@
 if(window.TOURS){
   const camliyayla=window.TOURS.find(t=>t.id==='camliyayla-doga-turu');if(camliyayla)camliyayla.image='/assets/camliyayla-kapak.jpg';
-  const baskonus=window.TOURS.find(t=>t.id==='baskonus-menzelet-ali-kayasi');if(baskonus)baskonus.image='/assets/baskonus-yaylasi-kapak.jpg';
   const doguEkspresi=window.TOURS.find(t=>t.id==='dogu-ekspresi-erzurum-kars-agri-van');if(doguEkspresi)doguEkspresi.image='/assets/tours/dogu-ekspresi-cover.avif';
   const nemrut=window.TOURS.find(t=>t.id==='nemrut-rumkale-gaziantep-turu');if(nemrut)nemrut.image='/assets/nemrut-rumkale-gaziantep-cover.jpg';
 }
