aae724e47f6dd325277aa78ca1eb3a31aa16155c
geziplatformu <futbolsovu@gmail.com>
2026-08-24 00:03:39 +0300

    Mevcut yerel tur kapaklarını önceliklendir

--- tour-images.js
+++ tour-images.js
@@ -1,6 +1,6 @@
 if(window.TOURS){
-  const camliyayla=window.TOURS.find(t=>t.id==='camliyayla-doga-turu');
-  if(camliyayla)camliyayla.image='https://sozsakarya.com/sites/1026/uploads/2026/03/07/dhbocyaxuae0dn8-1772840663.jpg';
-  const doguEkspresi=window.TOURS.find(t=>t.id==='dogu-ekspresi-erzurum-kars-agri-van');
-  if(doguEkspresi)doguEkspresi.image='/assets/tours/dogu-ekspresi-cover.avif';
+  const camliyayla=window.TOURS.find(t=>t.id==='camliyayla-doga-turu');if(camliyayla)camliyayla.image='/assets/camliyayla-kapak.jpg';
+  const baskonus=window.TOURS.find(t=>t.id==='baskonus-menzelet-ali-kayasi');if(baskonus)baskonus.image='/assets/baskonus-yaylasi-kapak.jpg';
+  const doguEkspresi=window.TOURS.find(t=>t.id==='dogu-ekspresi-erzurum-kars-agri-van');if(doguEkspresi)doguEkspresi.image='/assets/tours/dogu-ekspresi-cover.avif';
+  const nemrut=window.TOURS.find(t=>t.id==='nemrut-rumkale-gaziantep-turu');if(nemrut)nemrut.image='/assets/nemrut-rumkale-gaziantep-cover.jpg';
 }
